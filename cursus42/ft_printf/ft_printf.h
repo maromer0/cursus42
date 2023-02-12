25a9bdee61636ba0785c65774bd997dcf0f947e3
Manuel Alejandro <maromero@student.42malaga.com>
2023-02-12 20:30:29 +0100

    crear make, .h y ft_printf

---
+++ cursus42/ft_printf/ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maromero <maromero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/12 18:17:47 by maromero          #+#    #+#             */
+/*   Updated: 2023/02/12 20:12:13 by maromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+int	ft_printf(char const *print, ...);
+
+#endif
