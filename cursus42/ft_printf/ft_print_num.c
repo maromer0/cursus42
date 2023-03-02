31fe68ee81ded1d89d6e3c3332c00a6cd000f3d5
Manuel Alejandro <maromero@student.42malaga.com>
2023-03-02 11:45:10 +0100

    Correcciones norminette, y tests paco

--- cursus42/ft_printf/ft_print_num.c
+++ cursus42/ft_printf/ft_print_num.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_num.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maromero <maromero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/02 11:30:49 by maromero          #+#    #+#             */
+/*   Updated: 2023/03/02 11:39:26 by maromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
 void	ft_print_num(int n, size_t *i)
 {
-	if(n == -2147483648)
+	if (n == -2147483648)
 	{
 		ft_print_num((n / 10), i);
 		ft_print_char('8', i);
@@ -14,7 +26,7 @@ void	ft_print_num(int n, size_t *i)
 	}
 	else
 	{
-		if (n >=10)
+		if (n >= 10)
 			ft_print_num((n / 10), i);
 		ft_print_char(('0' + n % 10), i);
 	}
--- cursus42/ft_printf/ft_printf.h
+++ cursus42/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: maromero <maromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 18:17:47 by maromero          #+#    #+#             */
-/*   Updated: 2023/02/12 20:12:13 by maromero         ###   ########.fr       */
+/*   Updated: 2023/03/02 11:38:57 by maromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,5 @@ void	ft_print_num(int n, size_t *i);
 void	ft_print_str(char *str, size_t *i);
 void	ft_print_uhex(unsigned int n, size_t *i);
 void	ft_print_unsig(unsigned int n, size_t *i);
-
 
 #endif
